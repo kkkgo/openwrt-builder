4242706015261bc09f16a0c57557fa4b333cfd91
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-05-15 00:37:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ mkdir -p /src/FILES/usr/bin/
 mv /src/clash /src/FILES/usr/bin/
 mkdir -p /src/cpfiles/
 cp -r /src/cpfiles/* /src/FILES/
+chmod +x /src/FILES/etc/*
+chmod +x /src/FILES/usr/bin/*
 echo PACKAGES="$pkg"
 make image PROFILE="generic" PACKAGES="$pkg" FILES="/src/FILES"
