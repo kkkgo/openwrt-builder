2ef3d635a6e7a59937c2f1498706b58992051c9c
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-05-14 18:33:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ grep -v "#" custom.config.sh | grep . >>/src/.config
 while read line; do
     pkg="$pkg $line"
 done <pkg.conf
-mkdir -p /src/builder/FILES/usr/bin/
-mv /src/clash /src/builder/FILES/usr/bin/
+mkdir -p /src/FILES/usr/bin/
+mv /src/clash /src/FILES/usr/bin/
 echo PACKAGES="$pkg"
 make image PROFILE="generic" PACKAGES="$pkg" FILES="/src/FILES"
