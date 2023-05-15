b8f022a1f0a3868d2818fc636dc8fcbda028a2c5
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-05-16 02:01:19 +0800

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -6,7 +6,7 @@ done </src/builder/download.pkg
 echo PACKAGES="$pkg"
 make image PROFILE="generic" PACKAGES="$pkg"
 rm /src/builder/bin/targets/x86/64/*
-# sed -i 's/package_reload:/package_reloads:/' Makefile
-# sed -i '/package_reloads:/i package_reload:\n\techo fake reload.\n\tmkdir -p /src/build_dir/target-x86_64_musl/root-x86//tmp/' Makefile
-# sed -i 's/checksum: FORCE/checksums:/' Makefile
-# sed -i '/checksums:/i checksum:\n\techo bypass checksum.\n' Makefile
+sed -i 's/package_reload:/package_reloads:/' Makefile
+sed -i '/package_reloads:/i package_reload:\n\techo fake reload.\n\tmkdir -p /src/build_dir/target-x86_64_musl/root-x86//tmp/' Makefile
+sed -i 's/checksum: FORCE/checksums:/' Makefile
+sed -i '/checksums:/i checksum:\n\techo bypass checksum.\n' Makefile
