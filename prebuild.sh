fd569a04bb20885cdb1c1fc3f11b2de11be8740b
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-05-16 16:38:19 +0800

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -10,3 +10,11 @@ sed -i 's/package_reload:/package_reloads:/' Makefile
 sed -i '/package_reloads:/i package_reload:\n\techo fake reload.\n\tmkdir -p /src/build_dir/target-x86_64_musl/root-x86//tmp/' Makefile
 sed -i 's/checksum: FORCE/checksums:/' Makefile
 sed -i '/checksums:/i checksum:\n\techo bypass checksum.\n' Makefile
+
+mkdir -p ./clash-dashboard
+git clone -b gh-pages --depth 1 https://github.com/Dreamacro/clash-dashboard ./clash-dashboard
+if [ -f ./clash-dashboard/index.html ]; then
+    rm -f /src/clash-dashboard/CNAME
+    rm -rf /src/clash-dashboard/.git
+# sed -i "s/\/settings/\//g" $(grep -rso "/settings" ~/clash-dashboard|cut -d":" -f1)
+fi
