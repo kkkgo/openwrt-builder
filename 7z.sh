46da77f133dcf172fdcd91da6149f2544d9b5816
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-05-21 15:45:04 +0800

    Update 7z.sh

--- 7z.sh
+++ 7z.sh
@@ -2,4 +2,4 @@
 cd /src/iso/ || exit
 source_dir="./"
 output_file="paopao-gateway-x86-64"$sha".7z"
-7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on "$output_file" "$source_dir"
+7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on -bsp1 -bso1 -bsp2 -bse1 -y "$output_file" "$source_dir"
