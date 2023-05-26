d97fa26e322440cf5ced3f862df69790e7a2c2cc
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-05-27 00:51:43 +0800

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -11,19 +11,20 @@ sed -i '/package_reloads:/i package_reload:\n\techo fake reload.\n\tmkdir -p /sr
 sed -i 's/checksum: FORCE/checksums:/' Makefile
 sed -i '/checksums:/i checksum:\n\techo bypass checksum.\n' Makefile
 
-# git clone -b gh-pages --depth 1 https://github.com/haishanh/yacd.git ./clash-dashboard
-# if [ -f ./clash-dashboard/index.html ]; then
-#     rm -f ./clash-dashboard/CNAME
-#     rm -rf ./clash-dashboard/.git
-#     mv ./clash-dashboard/index.html ./clash-dashboard/index_base.html
-#     # sed -i "s/\/settings/\//g" $(grep -rso "/settings" ./clash-dashboard | cut -d":" -f1)
-# fi
+mkdir -p /src/builder/clash-premium/
 
 # mmdb
-curl -sLo ./Country.mmdb https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country-only-cn-private.mmdb
-mmdb_hash=$(sha256sum ./Country.mmdb | grep -Eo "[a-zA-Z0-9]{64}" | head -1)
+curl -sLo /src/builder/Country.mmdb https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country-only-cn-private.mmdb
+mmdb_hash=$(sha256sum /src/builder/Country.mmdb | grep -Eo "[a-zA-Z0-9]{64}" | head -1)
 mmdb_down_hash=$(curl -s https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country-only-cn-private.mmdb.sha256sum | grep -Eo "[a-zA-Z0-9]{64}" | head -1)
 if [ "$mmdb_down_hash" != "$mmdb_hash" ]; then
     cp /mmdb_down_hash_error .
     exit
 fi
+
+cd /src/builder/build_dir/target-x86_64_musl/root-x86/lib/preinit || exit
+rm 30_failsafe_wait
+rm 10_indicate_failsafe
+rm 40_run_failsafe_hook
+rm 99_10_failsafe_dropbear
+rm 99_10_failsafe_login
