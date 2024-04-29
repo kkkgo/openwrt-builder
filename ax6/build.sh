bb26863d9b9811a7afb27d7a24b75ac1206b2280
kissshot <4530897+kkkgo@users.noreply.github.com>
2024-04-29 18:50:59 +0800

    immax6

--- ax6/build.sh
+++ ax6/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 chmod +x /src/files/usr/bin/*
 
-# use mosdns
+# remove hijack udp 53
 dnsmasqfile=/src/package/network/services/dnsmasq/files/dnsmasq.init
+sed -i 's/iptables /echo -n #/g' "$dnsmasqfile"
+sed -i 's/ip6tables /echo -n #/g' "$dnsmasqfile"
+# use mosdns
 sed -i 's/append_parm "\$cfg" port "--port"/append_parm "\$cfg" 0 "--port"/g' "$dnsmasqfile"
 sed -i '/start_service()/a \
 if ps |grep -v "grep"|grep -q mosdns;then\
@@ -12,8 +15,8 @@ echo "Start mosdns..."\
 mosdns start -d /etc -c mosdns.yaml &\
 fi' "$dnsmasqfile"
 
-# patch Makefile
-sed -i "s/e2fsprogs//g" /src/target/linux/qualcommax/Makefile
-sed -i "s/ kmod-fs-ext4//g" /src/target/linux/qualcommax/Makefile
-sed -i "s/ uboot-envtools//g" /src/target/linux/qualcommax/Makefile
-make -j1 V=sc
+# luci settings
+luciset=/src/package/emortal/default-settings/files/99-default-settings-chinese
+sed -i '/system.@system\[0\].zonename/a set system.@system[0].hostname="Router"' "$luciset"
+
+make -j4 V=sc
