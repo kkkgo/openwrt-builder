298548faf01c53ddba355989300c3ebf051d0fa1
kissshot <4530897+kkkgo@users.noreply.github.com>
2024-04-30 15:14:48 +0800

    remove bridger

--- 6088/ib.sh
+++ 6088/ib.sh
@@ -4,4 +4,5 @@ while read line; do
     pkg="$pkg $line"
 done </src/ib.pkg
 echo PACKAGES="$pkg"
+sed -i "s/ bridger//g" /src/target/linux/mediatek/filogic/target.mk
 make image PROFILE=tplink_tl-xdr6088 PACKAGES="$pkg" FILES="files" 
