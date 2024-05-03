fe066995121c564b05fa27c6266c6f0a0b0db137
kissshot <4530897+kkkgo@users.noreply.github.com>
2024-05-03 20:09:50 +0800

    j4

--- imm/make.sh
+++ imm/make.sh
@@ -70,7 +70,7 @@ sed -i 's/set dhcp.lan.ra_slaac=1/set dhcp.lan.ra_slaac=0/' $odhcpd
 sed -i '/set dhcp.lan.ra_slaac=0/a set dhcp.lan.dns_service=0\nadd_list dhcp.lan.ra_flags=none' $odhcpd
 
 
-make -j5
+make -j4
 tree /src/bin/targets/
 # pack bin
 mkdir -p /data
