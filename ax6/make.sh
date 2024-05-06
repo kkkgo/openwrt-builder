4fd19220de306b217303e6f6d7ac81bca20ff445
kissshot <4530897+kkkgo@users.noreply.github.com>
2024-05-06 12:13:23 +0800

    -b 5.15-qsdk11

--- ax6/make.sh
+++ ax6/make.sh
@@ -8,3 +8,12 @@ make download -j4
 
 make -j5 V=s
 tree /src/bin/targets/
+
+mkdir -p /data
+# rm /src/bin/targets/qualcommax/ipq807x/*.zst
+rm -rf /src/bin/targets/qualcommax/ipq807x/packages
+
+7z a -t7z -mx=9 /data/ax6.7z /src/bin/targets/qualcommax/ipq807x
+
+# make clean
+# rm -rf /src/.git
