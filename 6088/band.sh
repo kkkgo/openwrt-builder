7a478d1ae9cb71d34992b5903c7632b7bc676ee7
kissshot <4530897+kkkgo@users.noreply.github.com>
2024-06-21 09:42:30 +0800

    cat test

--- 6088/band.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "BAND_NAME=${{ github.event.inputs.BAND_NAME }}" > band.txt
-echo "BAND_NAME_ADDMAC=${{ github.event.inputs.BAND_NAME_ADDMAC }}" >> band.txt
-echo "BAND_SSID=${{ github.event.inputs.BAND_SSID }}" >> band.txt
-echo "BAND_ROOT_PASS=${{ github.event.inputs.BAND_ROOT_PASS }}" >> band.txt
-echo "BAND_WLAN_PASS=${{ github.event.inputs.BAND_WLAN_PASS }}" >> band.txt
-echo "PASS_PUBKEY=${{ github.event.inputs.PASS_PUBKEY }}" >> band.txt
-echo "BAND_CIDR=${{ github.event.inputs.BAND_CIDR }}" >> band.txt
